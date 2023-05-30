6a903077c2b4c7f34ba55a7b2717d40c9534645c
yeomjinseop <yeomjinseop@gmail.com>
2023-05-30 11:27:33 +0900

    get user BaekJoon Data

---
+++ .history/app_20230530110957.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+
+const userBeakJoonIds = ['yeom123']
+
+app.get('/rank', async (req, res) => {
+  try {
+    const usersData = await Promise.all(userBeakJoonIds.map(async (userId) => {
+      const options = {
+        method: 'GET',
+        url: 'https://solved.ac/api/v3/search/user',
+        params: {query: 'yeom123'},
+        headers: {Accept: 'application/json'}
+      };
+  
+      const { data } = await axios.request(options);
+      return data;
+    }))
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+
+app.listen(8080);
---
+++ .history/app_20230530111805.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+
+const userBaekJoonIds = ['yeom123']
+
+app.get('/rank', async (req, res) => {
+  try {
+    const usersData = await Promise.all(userBaekJoonIds.map(async (userId) => {
+      const options = {
+        method: 'GET',
+        url: 'https://solved.ac/api/v3/search/user',
+        params: {query: userId},
+        headers: {Accept: 'application/json'}
+      };
+  
+      const { data } = await axios.request(options);
+      console.log(...data.items);
+      return data;
+    }))
+    res.send(usersData);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+
+app.listen(8080);
